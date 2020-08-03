47dc80ed4af390b148274fa62a64212d8a4c40e3
zkj <438101845@qq.com>
2020-08-03 23:21:24 +0800

    [zkj]-feat: when word's vowels more than 0.3 and continuous

--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -16,4 +16,11 @@ public class MommifierTest {
         String result = mommifier.insertMommy("appple");
         assertEquals(result, "appple");
     }
+
+    @Test
+    void should_return_input_when_insert_mommy_given_string_of_vowels_more_than_30_percent_and_continuous() {
+        Mommifier mommifier = new Mommifier();
+        String result = mommifier.insertMommy("appeeple");
+        assertEquals(result, "appeemommyple");
+    }
 }
